4b40de0a236e2eca8d422671343235e74438be34
uniflare <uniflare@gmail.com>
2017-10-07 20:41:26 +0100

    !R (Plugin) Renamed CVARS and changed textures
    !T (assets) Changed/Normalized texture files
    Added new DXT1/ETC2 (SF_Image_nonpower2) textures with different graphics
    Renamed textures for clarity
    
    !T (CVARS) Normalized CVARS for clarity
    Renamed some CVARS to make it easier to differentiate between main splash and initial
    Changed the default values of the texture paths to reflect the changed texture filenames
    
    !T (CFG) Updated config file due to previous changes

--- Code/cvars.h
+++ Code/cvars.h
@@ -43,14 +43,14 @@ struct CVars
 		assert(gEnv->pConsole);
 		if (!gEnv->pConsole) return;
 		
-		gEnv->pConsole->UnregisterVariable("splash_texture_a");
+		gEnv->pConsole->UnregisterVariable("splash_texture_initial");
 		gEnv->pConsole->UnregisterVariable("splash_texture");
 		gEnv->pConsole->UnregisterVariable("splash_startTimeOffset");
 		gEnv->pConsole->UnregisterVariable("splash_minimumPlaybackTime");
-		gEnv->pConsole->UnregisterVariable("splash_minimumPlaybackTimeA");
+		gEnv->pConsole->UnregisterVariable("splash_minimumPlaybackTime_initial");
 		gEnv->pConsole->UnregisterVariable("splash_show_initial");
-		gEnv->pConsole->UnregisterVariable("splash_show_main");
 		gEnv->pConsole->UnregisterVariable("splash_show");
+		gEnv->pConsole->UnregisterVariable("splash_plugin_enable");
 
 		CRY_LOG_DEBUG("Unregistered CVars.");
 	}
@@ -60,14 +60,14 @@ struct CVars
 		assert(gEnv->pConsole);
 		if (!gEnv->pConsole) return false;
 
-		REGISTER_CVAR2("splash_show", &m_iSplashEnable, 1, VF_CHEAT, "Whether to enable the splash plugin (0=no)");
-		REGISTER_CVAR2("splash_show_main", &m_iMainSplashEnable, 1, VF_CHEAT, "Whether to enable the main (second) splash image (0=no)");
-		REGISTER_CVAR2("splash_show_initial", &m_iInitialSplashEnable, 1, VF_CHEAT, "Whether to enable the intial splash screen (0=no)");
-		REGISTER_CVAR2("splash_minimumPlaybackTimeA", &m_fInitialSplashPlaybackTime, 5.f, VF_CHEAT, "Initial Splash - Minimum playback time in seconds (float min 0.f)");
-		REGISTER_CVAR2("splash_minimumPlaybackTime", &m_fSplashPlaybackTime, 10.f, VF_CHEAT, "Main Splash - Minimum playback time in seconds (float min 0.f)");
+		REGISTER_CVAR2("splash_plugin_enable", &m_iSplashEnable, 1, VF_CHEAT, "Whether to enable the splash plugin (0=no)");
+		REGISTER_CVAR2("splash_show", &m_iMainSplashEnable, 1, VF_CHEAT, "Whether to enable the main (second) splash image (0=no)");
+		REGISTER_CVAR2("splash_show_initial", &m_iInitialSplashEnable, 1, VF_CHEAT, "Whether to enable the intial (smaller) splash screen (0=no)");
+		REGISTER_CVAR2("splash_minimumPlaybackTime_initial", &m_fInitialSplashPlaybackTime, 3.f, VF_CHEAT, "Initial Splash - Minimum playback time in seconds (float min 0.f)");
+		REGISTER_CVAR2("splash_minimumPlaybackTime", &m_fSplashPlaybackTime, 3.f, VF_CHEAT, "Main Splash - Minimum playback time in seconds (float min 0.f)");
 		REGISTER_CVAR2("splash_startTimeOffset", &m_fStartTimeOffset, 0.0f, VF_CHEAT, "Offset to make splash_minimumPlaybackTime more accurate (float)");
-		m_pSplashTexturePath = REGISTER_STRING("splash_texture", "splashexample/textures/splash.dds", VF_CHEAT, "Sets the splash overlay texture to load");
-		m_pInitialSplashTexturePath = REGISTER_STRING("splash_texture_a", "splashexample/textures/splash_a.dds", VF_CHEAT, "Sets the initial splash overlay texture to load (before we go fullscreen)");
+		m_pSplashTexturePath = REGISTER_STRING("splash_texture", "splashexample/textures/Splash.dds", VF_CHEAT, "Sets the splash overlay texture to load");
+		m_pInitialSplashTexturePath = REGISTER_STRING("splash_texture_initial", "splashexample/textures/InitialSplash.dds", VF_CHEAT, "Sets the initial splash overlay texture to load (before we go fullscreen)");
 
 		CRY_LOG_DEBUG("Registered CVars.");
 		return true;
